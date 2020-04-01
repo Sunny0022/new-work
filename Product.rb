57d35aabfbc5806acda05bace5e641063ddb677d
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-04-01 15:56:34 +0530

    Update Product.rb

--- Product.rb
+++ Product.rb
@@ -5,13 +5,13 @@ class Product
   attr_accessor :item_name, :price, :exempt, :imported, :tax
 
   def initialize(item_name, price, exempt, imported)
-	  @item_name = item_name
-	  @price = price.to_i
-	  @exempt = exempt
-	  @imported = imported
-	  @tax = 0
+    @item_name = item_name
+    @price = price.to_i
+    @exempt = exempt
+    @imported = imported
+    @tax = 0
   end
-
+	
   def calculate_tax
     if !@exempt && @imported
       @tax = @price * 0.15
