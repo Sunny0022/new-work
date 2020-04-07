34f32672a12727b162a0aa85df50900ba31e8e99
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-04-07 12:37:19 +0530

    Add files via upload

---
+++ product.rb
@@ -0,0 +1,33 @@
+require_relative "receipt.rb"
+
+IMPORT_SALES_CONSTANT = 0.15
+SALES_TAX_CONSTANT = 0.1
+class Product
+  attr_accessor :item_name, :price, :exempt, :imported, :tax
+
+  def initialize(item_name, price, exempt, imported)
+	  @item_name = item_name
+	  @price = price.to_i
+	  @exempt = exempt
+	  @imported = imported
+    @tax = 0
+  end
+
+  def calculate_tax
+    if !@exempt && @imported
+      @tax = @price * IMPORT_SALES_CONSTANT
+    elsif !@exempt && !@imported
+      @tax = @price * SALES_TAX_CONSTANT
+    elsif @exempt && !@imported
+      @tax = 0
+    end
+  end
+end
+prod1 = Product.new('Chocolates', 120, true, true)
+prod2 = Product.new('Potato Chips', 100, true, false)
+prod3 = Product.new('Perfume', 150, false, true)
+receipt = Receipt.new
+receipt.add_product(prod1)
+receipt.add_product(prod2)
+receipt.add_product(prod3)
+receipt.display_product
---
+++ receipt.rb
@@ -0,0 +1,26 @@
+class Receipt
+
+  def initialize
+    @list = Array.new
+    @price_total = 0
+    @tax_total = 0
+    @grand_total = 0
+  end
+
+  def add_product(item)
+    @list << item
+    item.calculate_tax
+    @price_total += item.price
+    @tax_total += item.tax
+    @grand_total = @price_total + @tax_total
+  end
+
+  def display_product
+    @list.each do |product|
+      puts "#{product.item_name}, #{product.price}"
+    end
+    puts "Subtotal: #{@price_total}"
+    puts "Tax Total: #{@tax_total}"
+    puts "Grand Total: #{@grand_total}"
+  end
+end
